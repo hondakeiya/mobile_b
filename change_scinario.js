9b6aa79a5fc4780e5e4542835f9f532b12bcb332
ひつじ <e195757@hitsujinoMacBook-Air.local>
2021-11-20 02:50:48 +0900

    羊の名前の変更ができるようになった

--- change_scinario.js
+++ change_scinario.js
@@ -1,49 +1,49 @@
 document.getElementById("change_scene")
 var current_id=0;
 var current_data=[];
+var target = "くっちー"
 
 disp_scenario();
 
 function disp_scenario(){
-    var result = scinario_data.filter(function(value){
-      if (value.id==current_id) return true;
-    })
-    if(result.length==0){return;}
+  var result = scinario_data.filter(function(value){
+    if (value.id==current_id) return true;
+  })
+  if(result.length==0){return;}
 
-    current_data=result[0];
-    document.forms.fl.choice1.value = current_data.choice[0].text;
-    if(current_data.choice.length>=2){
-      document.forms.fl.choice2.value = current_data.choice[1].text;
-      document.forms.fl.choice2.style.visibility = "visible";
-    }else{
-      document.forms.fl.choice2.style.visibility = "hidden";
-    }if (current_data.choice.length>=3){
-      document.forms.fl.choice3.value = current_data.choice[2].text;
-      document.forms.fl.choice3.style.visibility = "visible";
-    }else{
-      document.forms.fl.choice3.style.visibility = "hidden";
-    }
-    bcg.src = "../images/"+current_data.img;
+  current_data=result[0];
+  document.forms.fl.choice1.value = current_data.choice[0].text;
+  if(current_data.choice.length>=2){
+    document.forms.fl.choice2.value = current_data.choice[1].text;
+    document.forms.fl.choice2.style.visibility = "visible";
+  }else{
+    document.forms.fl.choice2.style.visibility = "hidden";
+  }if (current_data.choice.length>=3){
+    document.forms.fl.choice3.value = current_data.choice[2].text;
+    document.forms.fl.choice3.style.visibility = "visible";
+  }else{
+    document.forms.fl.choice3.style.visibility = "hidden";
+  }
+  bcg.src = "../images/"+current_data.img;
 
-    if (current_data.id == 2){
-        const input1 = document.createElement("input");
-        input1.setAttribute("type", "text");
-        input1.setAttribute("value", "初期表示");
-        player_name = window.prompt("ユーザー名を入力してください", "");
-        current_data.msg = "なるほど!"+player_name+"というんだな！次に初めてふれる羊の名前を教えてくれ!";
-    }
-    if(current_data.id == 3){
-        const input2 = document.createElement("input");
-        input2.setAttribute("type", "text");
-        input2.setAttribute("value", "初期表示");
-        hituji_name = window.prompt("羊の名前を入力してください", "");
-    }
-    /*入力した羊の名前でシナリオ内の仮称くっちーを置き換えたいけどうまくいかない
-    for (var i=current_data.id;i<current_data.id.length;i++){
-        current_data.msg = current_data.msg.replase("くっちー", hituji_name);
-    }
-    */
-    msg.innerHTML = current_data.msg;
+  if (current_data.id == 2){
+    const input1 = document.createElement("input");
+    input1.setAttribute("type", "text");
+    input1.setAttribute("value", "初期表示");
+    player_name = window.prompt("ユーザー名を入力してください", "");
+    current_data.msg = "なるほど!"+player_name+"というんだな！次に初めてふれる羊の名前を教えてくれ!";
+  }
+  if(current_data.id == 3){
+    const input2 = document.createElement("input");
+    input2.setAttribute("type", "text");
+    input2.setAttribute("value", "初期表示");
+    hituji_name = window.prompt("羊の名前を入力してください", "");
+  }
+  //入力した羊の名前でシナリオ内の仮称くっちーを置き換えたいけどうまくいかない
+  if(current_data.id>4){
+    current_data.msg = current_data.msg.replace("くっちー", hituji_name);
+  }
+  msg.innerHTML = current_data.msg;
 }
 
 function change_scenario(selectno) {
